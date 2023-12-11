b844e8f90702be1363a8aca74c8ab97f5195d27e
Kafonek <kafonek@gmail.com>
2023-12-11 10:09:13 -0500

    Python day09 done

--- python/aoc_2023/day09.py
+++ python/aoc_2023/day09.py
@@ -1,12 +1,23 @@
-from dataclasses import dataclass
 from typing import List
 
 
-def make_diffs(numbers: List[int]):
+def make_diffs(numbers: List[int]) -> List[int]:
+    """
+    Return a list of size one less than input numbers, is the diff between each number
+
+    numbers = [1, 2, 3]
+    assert make_diffs(numbers) == [1, 1, 1]
+    """
+    
     return [j - i for i, j in zip(numbers, numbers[1:])]
 
+def make_diff_hierarchy(numbers: List[int]) -> List[List[int]]:
+    """
+    Return a list of lists for all diffs until len(last_diff) == 1
 
-def make_diff_hierarchy(numbers: List[int]):
+    numbers = [1, 1, 2, 4]
+    assert make_diff_hierarchy(numbers) == [[0, 1, 2], [1, 1], [0]]
+    """
     diffs = make_diffs(numbers)
     total_diffs = [diffs]
     while len(diffs) > 1:
@@ -14,16 +25,32 @@ def make_diff_hierarchy(numbers: List[int]):
         total_diffs.append(diffs)
     return total_diffs
 
+def next_diffs(diff_hierarchy: List[List[int]]) -> List[int]:
+    """
+    Given a diff hierarchy, return a list of 'next results'
 
-@dataclass
-class Pattern:
-    numbers: List[int]
+    diff_hiearchy = [[0, 1, 2], [1, 1], [0]]
+    assert next_diffs(diff_hierarchy) == [3, 1]
+    """
+    results = []
+    to_add = diff_hierarchy[-1][-1] # should always be 0 in theory
+    for diff in reversed(diff_hierarchy[:-1]):
+        value = diff[-1] + to_add
+        results.append(value)
+        to_add = value
+    return results
 
-    @staticmethod
-    def from_line(s: str):
-        return Pattern([int(c) for c in s.split()])
+def previous_diffs(diff_hierarchy: List[List[int]]):
+    """
+    Given a diff hierarchy, return a list of 'next results'
 
-    def next_number(self):
-        diff_hierarchy = make_diff_hierarchy(self.numbers)
-        changes = sum([item[-1] for item in diff_hierarchy])
-        return self.numbers[-1] + changes
+    diff_hiearchy = [[0, 1, 2], [1, 1], [0]]
+    assert next_diffs(diff_hierarchy) == [-1, 1]]
+    """
+    results = []
+    to_subtract = diff_hierarchy[-1][-1] # should always be 0 in theory
+    for diff in reversed(diff_hierarchy[:-1]):
+        value = diff[0] - to_subtract
+        results.append(value)
+        to_subtract = value
+    return results
--- src/bin/day09-1.py
+++ src/bin/day09-1.py
@@ -1,14 +1,18 @@
-from aoc_2023.day09 import Pattern
-from aoc_2023.utils import run_and_time
 from pathlib import Path
+
+from aoc_2023.day09 import make_diff_hierarchy, next_diffs
+from aoc_2023.utils import run_and_time
 
 
 def solve(fp: Path) -> str:
     lines = fp.read_text().split("\n")
-    patterns = [Pattern.from_line(line) for line in lines]
     answer = 0
-    for pattern in patterns:
-        answer += pattern.next_number()
+    for line in lines:
+        nums = [int(c) for c in line.split()]
+        diffs = make_diff_hierarchy(nums)
+        nd = next_diffs(diffs)
+        next_number = nums[-1] + nd[-1]
+        answer += next_number
     return str(answer)
 
 
